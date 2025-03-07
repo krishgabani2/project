0e6673192981fe11e11ea190de536336f0071d4e
unknown <krishgabani2@gmail.com>
2025-03-07 19:20:03 +0530

    Fixed NLTK punkt tokenizer issue

--- expenses/views.py
+++ expenses/views.py
@@ -12,6 +12,7 @@ import requests
 import nltk
 nltk.data.path.append("/opt/render/nltk_data")    # Explicitly set path
 nltk.download("stopwords", download_dir="/opt/render/nltk_data")
+nltk.download("punkt", download_dir="/opt/render/nltk_data")
 
 from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))  # This should now work
